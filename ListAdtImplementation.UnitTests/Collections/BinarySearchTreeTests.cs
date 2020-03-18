f3467a189e730295a65079289223d9f6e7a319df
Gustavo Passos <theguspassos@gmail.com>
2020-03-18 20:12:32 -0300

    add tests to binary search tree

---
+++ ListAdtImplementation.UnitTests/Collections/BinarySearchTreeTests.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using ListAdtImplementation.Collections;
+using NUnit.Framework;
+
+namespace ListAdtImplementation.UnitTests.Collections
+{
+    [TestFixture]
+    public class BinarySearchTreeTests 
+    {
+        [TestFixture]
+        public class Adding
+        {
+            [TestFixture]
+            public class AddInEmptyTree
+            {
+                private BinarySearchTree<int> binarySearchTree;
+                private const int valueAdded = 1;
+
+                [OneTimeSetUp]
+                public void AddToEmptyList()
+                {
+                    binarySearchTree = new BinarySearchTree<int>();
+                    binarySearchTree.Add(valueAdded);
+                }
+
+                [Test]
+                public void ShouldSetAsRoot()
+                    => binarySearchTree.Root.Value.Should().Be(valueAdded);
+
+                [Test]
+                public void RootShouldBeLeaf()
+                    => binarySearchTree.Root.IsLeaf().Should().BeTrue();
+            }
+        }
+    }
+}
---
+++ ListAdtImplementation/Collections/BinarySearchTree.cs
@@ -0,0 +1,20 @@
+﻿namespace ListAdtImplementation.Collections
+{
+    public class BinarySearchTree<T>
+    {
+        public Node Root { get; private set; }
+
+        public void Add(T node)
+        {
+        }
+
+        public class Node
+        {
+            public Node Left { get; private set; }
+            public Node Right { get; private set; }
+            public T Value { get; private set; }
+
+            public bool IsLeaf() => Left == null && Right == null;
+        }
+    }
+}
