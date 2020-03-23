caec8f633a00bcfac44ce6676299cbd0da64f063
Gustavo Passos <theguspassos@gmail.com>
2020-03-23 20:57:14 -0300

    add function to add node in binary tree

--- ListAdtImplementation.UnitTests/Collections/BinarySearchTreeTests.cs
+++ ListAdtImplementation.UnitTests/Collections/BinarySearchTreeTests.cs
@@ -31,6 +31,77 @@ namespace ListAdtImplementation.UnitTests.Collections
                 public void RootShouldBeLeaf()
                     => binarySearchTree.Root.IsLeaf().Should().BeTrue();
             }
+            
+            [TestFixture]
+            public class AddSameAsRoot
+            {
+                private BinarySearchTree<int> binarySearchTree;
+                private const int rootValue = 1;
+
+                [OneTimeSetUp]
+                public void AddToEmptyList()
+                {
+                    binarySearchTree = new BinarySearchTree<int>();
+                    binarySearchTree.Add(rootValue);
+                    binarySearchTree.Add(rootValue);
+                }
+
+                [Test]
+                public void ShouldNotChangeRoot()
+                    => binarySearchTree.Root.Value.Should().Be(rootValue);
+
+                [Test]
+                public void RootShouldStillBeLeaf()
+                    => binarySearchTree.Root.IsLeaf().Should().BeTrue();
+            }
+
+            [TestFixture]
+            public class AddGreaterThanRoot
+            {
+                private BinarySearchTree<int> binarySearchTree;
+                private const int rootValue = 1;
+                private const int valueAdded = 2;
+
+                [OneTimeSetUp]
+                public void AddGreaterValue()
+                {
+                    binarySearchTree = new BinarySearchTree<int>();
+                    binarySearchTree.Add(rootValue);
+                    binarySearchTree.Add(valueAdded);
+                }
+
+                [Test]
+                public void ShouldSetRightToRoot()
+                    => binarySearchTree.Root.Right.Value.Should().Be(valueAdded);
+
+                [Test]
+                public void RootShouldBeOriginalValue()
+                    => binarySearchTree.Root.Value.Should().Be(rootValue);
+            }
+
+            [TestFixture]
+            public class AddLessThanRoot
+            {
+                private BinarySearchTree<int> binarySearchTree;
+                private const int rootValue = 1;
+                private const int valueAdded = -1;
+
+                [OneTimeSetUp]
+                public void AddLessThanRott()
+                {
+                    binarySearchTree = new BinarySearchTree<int>();
+                    binarySearchTree.Add(rootValue);
+                    binarySearchTree.Add(valueAdded);
+                }
+
+                [Test]
+                public void ShouldSetLeftToRoot()
+                    => binarySearchTree.Root.Left.Value.Should().Be(valueAdded);
+
+                [Test]
+                public void RootShouldBeOriginalValue()
+                    => binarySearchTree.Root.Value.Should().Be(rootValue);
+            }
         }
     }
 }
--- ListAdtImplementation/Collections/BinarySearchTree.cs
+++ ListAdtImplementation/Collections/BinarySearchTree.cs
@@ -1,18 +1,37 @@
-﻿namespace ListAdtImplementation.Collections
+﻿using System;
+
+namespace ListAdtImplementation.Collections
 {
-    public class BinarySearchTree<T>
+    public class BinarySearchTree<T> where T : IComparable
     {
         public Node Root { get; private set; }
 
-        public void Add(T node)
+        public void Add(T value)
         {
+            if (Root == null)
+                Root = new Node { Value = value };
+
+            if (Root.Value.CompareTo(value) == 0)
+                return;
+
+            if (value.CompareTo(Root.Value) > 0)
+            {
+                var newNode = new Node { Value = value, Parent = Root };
+                Root.Right = newNode;
+            }
+            else
+            {
+                var newNode = new Node { Value = value, Parent = Root };
+                Root.Left = newNode;
+            }
         }
 
         public class Node
         {
-            public Node Left { get; private set; }
-            public Node Right { get; private set; }
-            public T Value { get; private set; }
+            public Node Left { get; set; }
+            public Node Right { get; set; }
+            public Node Parent { get; set; }
+            public T Value { get; set; }
 
             public bool IsLeaf() => Left == null && Right == null;
         }
