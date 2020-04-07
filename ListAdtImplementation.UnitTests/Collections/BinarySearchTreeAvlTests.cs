55a75c51efa51459a69c74dd6277668b918f21a1
Gustavo Passos <theguspassos@gmail.com>
2020-04-07 15:11:49 -0300

    add method to delete in avl tree

--- ListAdtImplementation.UnitTests/Collections/BinarySearchTreeAvlTests.cs
+++ ListAdtImplementation.UnitTests/Collections/BinarySearchTreeAvlTests.cs
@@ -1,9 +1,6 @@
 ﻿using FluentAssertions;
 using ListAdtImplementation.Collections;
 using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace ListAdtImplementation.UnitTests.Collections
 {
@@ -33,7 +30,7 @@ namespace ListAdtImplementation.UnitTests.Collections
                 public void RootShouldBeLeaf()
                     => binarySearchTree.Root.IsLeaf().Should().BeTrue();
             }
-            
+
             [TestFixture]
             public class AddSameAsRoot
             {
@@ -104,43 +101,90 @@ namespace ListAdtImplementation.UnitTests.Collections
                 public void RootShouldBeOriginalValue()
                     => binarySearchTree.Root.Value.Should().Be(rootValue);
             }
+        }
+
+        [TestFixture]
+        public class Deleting
+        {
+            [Test]
+            public void ShouldDoNothingInEmptyTree()
+            {
+                var binaryTree = new BinarySearchTreeAvl<int>();
+                binaryTree.Remove(1);
+                binaryTree.Root.Should().BeNull();
+            }
+
+            [Test]
+            public void ShouldDeleteRootWhenIsRoot()
+            {
+                var binaryTree = new BinarySearchTreeAvl<int>();
+                binaryTree.Add(1);
+                binaryTree.Remove(1);
+                binaryTree.Root.Should().BeNull();
+            }
+
+            [Test]
+            public void ShouldRemoveNodeIfIsLeaf()
+            {
+                var binaryTree = new BinarySearchTreeAvl<int>();
+                binaryTree.Add(1);
+                binaryTree.Add(2);
+                binaryTree.Remove(2);
+                binaryTree.Root.IsLeaf().Should().BeTrue();
+            }
 
             [TestFixture]
-            public class AddMultiple
+            public class DeleteWithOneChild
             {
-                private BinarySearchTreeAvl<int> binarySearchTree;
-                private const int rootValue = 5;
+                private BinarySearchTreeAvl<int> binaryTree;
 
                 [OneTimeSetUp]
-                public void AddMultipleValues()
+                public void CreateTree()
                 {
-                    binarySearchTree = new BinarySearchTreeAvl<int>();
-                    binarySearchTree.Add(rootValue);
-                    binarySearchTree.Add(rootValue - 1);
-                    binarySearchTree.Add(rootValue - 2);
-                    binarySearchTree.Add(rootValue + 1);
-                    binarySearchTree.Add(rootValue + 2);
+                    binaryTree = new BinarySearchTreeAvl<int>();
+                    binaryTree.Add(1);
+                    binaryTree.Add(2);
+                    binaryTree.Add(3);
+                    binaryTree.Remove(2);
                 }
 
                 [Test]
-                public void RootShouldBeRootValue()
-                    => binarySearchTree.Root.Value.Should().Be(rootValue);
+                public void RemovedShouldNotExistInTree()
+                    => binaryTree.Contains(2).Should().BeFalse();
 
                 [Test]
-                public void RootLeftShouldBeRootMinusOne()
-                    => binarySearchTree.Root.Left.Value.Should().Be(rootValue - 1);
+                public void HeadRightChildShouldBeDeletedChild()
+                    => binaryTree.Root.Right.Value.Should().Be(3);
+            }
+
+            [TestFixture]
+            public class DeleteWithTwoChildren
+            {
+                private BinarySearchTree<int> binaryTree;
+
+                [OneTimeSetUp]
+                public void CreateTree()
+                {
+                    binaryTree = new BinarySearchTree<int>();
+                    binaryTree.Add(1);
+                    binaryTree.Add(3);
+                    // 3 children
+                    binaryTree.Add(2);
+                    binaryTree.Add(4);
+                    binaryTree.Remove(3);
+                }
 
                 [Test]
-                public void RootLeftLeftShouldBeRootMinusTwo()
-                    => binarySearchTree.Root.Left.Left.Value.Should().Be(rootValue - 2);
+                public void DeletedShouldNotExist()
+                    => binaryTree.Contains(3).Should().BeFalse();
 
                 [Test]
-                public void RootRightShouldBeRootPlusOne()
-                    => binarySearchTree.Root.Right.Value.Should().Be(rootValue + 1);
+                public void DeletedShouldBeReplacedByMinRightChild()
+                    => binaryTree.Root.Right.Value.Should().Be(4);
 
                 [Test]
-                public void RootRightRightShouldBeRootPlusTwo()
-                    => binarySearchTree.Root.Right.Right.Value.Should().Be(rootValue + 2);
+                public void NewDeletedShouldHaveChildFromRemoved()
+                    => binaryTree.Root.Right.Left.Value.Should().Be(2);
             }
         }
     }
--- ListAdtImplementation/Collections/BinarySearchTreeAvl.cs
+++ ListAdtImplementation/Collections/BinarySearchTreeAvl.cs
@@ -23,13 +23,66 @@ namespace ListAdtImplementation.Collections
             return node;
         }
 
+        public void Remove(T value) => Root = Remove(value, Root);
+
+        public Node Remove(T value, Node node)
+        {
+            if (node == null) return null;
+
+            int diff = value.CompareTo(node.Value);
+            if (diff == 0)
+            {
+                if (node.IsLeaf()) return null;
+                else if (node.HasBothChildren())
+                {
+                    var successor = FindMin(node.Right);
+                    node.Value = successor.Value;
+                    node.Right = Remove(successor.Value, node.Right);
+                }
+                else if (node.HasChildren()) return node.Left ?? node.Right;
+            }
+
+            if (diff < 0)
+                node.Left = Remove(value, node.Left);
+            else if (diff > 0)
+                node.Right = Remove(value, node.Right);
+
+            return node;
+        }
+
+        public bool Contains(T value)
+        {
+            var currentNode = Root;
+            while (currentNode != null)
+            {
+                int diff = value.CompareTo(currentNode.Value);
+                if (diff == 0) return true;
+                if (diff < 0) currentNode = currentNode.Left;
+                else currentNode = currentNode.Right;
+            }
+
+            return false;
+        }
+
+        private Node FindMin(Node node)
+        {
+            if (node == null) return null;
+            if (node.Left == null) return node;
+
+            var currentNode = node.Left;
+            while (currentNode.Left != null)
+                currentNode = currentNode.Left;
+
+            return currentNode;
+        }
+
         public class Node
         {
             public T Value { get; set; }
             public Node Right { get; set; }
             public Node Left { get; set; }
             public int Height { get; set; }
-            
+
             public bool IsLeaf() => Left == null && Right == null;
             public bool HasBothChildren() => Left != null && Right != null;
             public bool HasChildren() => Left != null || Right != null;
